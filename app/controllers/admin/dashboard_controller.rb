d1fbf796129b27d6fd22b8c3042796b606a4a2fe
Tristan <tristandeering@hotmail.com>
2018-12-09 00:27:10 +0000

    got hardcoded auth working

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admin::DashboardController < ApplicationController
+  # puts ENV['ADMIN_AUTH_USERNAME'].inspect
+  # puts ENV['ADMIN_AUTH_PASSWORD'].inspect
+  # FIXME: Make these use dotenv. Ask a mentor for help on Monday
+  http_basic_authenticate_with name: "Jungle", password: "book"
   def show
   end
 end
